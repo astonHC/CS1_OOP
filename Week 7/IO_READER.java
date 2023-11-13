e9b19f1ca7f80d10e8b246c6288a209ff5b70267
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-11-13 12:23:51 +0000

    added unit testing

--- Week 7/IO_READER.java
+++ Week 7/IO_READER.java
@@ -11,23 +11,37 @@
 /* NESTED INCLUDES */
 
 import java.util.ArrayList;
+import java.io.*;
 
 class CONTACT
 {
-    private static String FORENAME;
-    private static String LASTNAME;
-    private static String EMAIL;
-    private static String PHONE_NO; 
+    private String FORENAME;
+    private String LASTNAME;
+    private String EMAIL;
+    private String PHONE_NO; 
 
     public CONTACT(String FORENAME, String LASTNAME)
     {
-        this.FORENAME = FORENAME;
-        this.LASTNAME = LASTNAME;
+        this.FORENAME += FORENAME;
+        this.LASTNAME += LASTNAME;
     }
 
-    public void SET_EMAIL(String EMAIL)
+    public final void SET_EMAIL(String EMAIL)
     {
-        this.EMAIL = EMAIL;
+        this.EMAIL += EMAIL;
+    }
+
+    public final void SET_PHONE_NO(String PHONE_NO)
+    {
+        this.PHONE_NO += PHONE_NO;
+    }
+
+    /* HANDLE IO EXCEPTION FOR UNIT TESTING PURPOSES TO ENSURE THAT ALL FIELDS ARE BEING */
+    /* ACCOUNTED FOR WHEN PARSING THE CSV */
+
+    public final String CONCATENATE_TYPE() throws IOException
+    {
+        return "Name: " + FORENAME + " " + LASTNAME + "\nEmail: " + EMAIL + "\nPhone Number: " + PHONE_NO; 
     }
 }
 
