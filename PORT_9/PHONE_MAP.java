ccb2c7fc4c909e94760efdd7108f36b1c33d4fe1
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-11-26 21:20:41 +0000

    Add files via upload

---
+++ PORT_9/PHONE_MAP.java
@@ -0,0 +1,21 @@
+/* COPYRIGHT (C) HARRY CLARK 2033 */
+
+/* CS1_OOP PORTFOLIO 9 */
+
+/* THIS FILE PERTAINS TOWARDS THE FUNCTIONALITY OF PROVIDING LIASSE BETWEEN A DATA SHEET */
+/* AND PROCESSING TO BE ABLE TO ASCERTAIN AN OUTCOME */
+
+/* SYSTEM INCLUDES */
+
+import java.util.HashMap;
+import java.util.ArrayList.*;
+
+public class PHONE_MAP
+{
+    private static final HashMap<String, Phone> PHONE_MAP = new HashMap<>();
+
+    public PHONE_MAP()
+    {
+        
+    }
+}
