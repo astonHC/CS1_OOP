783501f4055f45163442078316674e749a21032e
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-11-27 00:24:17 +0000

    Add files via upload

---
+++ PORT_9/PHONE_LINES.java
@@ -0,0 +1,26 @@
+/* COPYRIGHT (C) HARRY CLARK 2023 */
+
+/* CS1_OOP PORTFOLIO 9 */
+
+/* THIS FILE PERTAINS TOWARDS THE FUNCTIONALITY OF PROVIDING LIASSE BETWEEN A DATA SHEET */
+/* AND PROCESSING TO BE ABLE TO ASCERTAIN AN OUTCOME */
+
+/* THIS FILE JUST SERVES TO PROVIDE A MORE CLEAN ENVIRONMENT TO HOUSE METHODS AND THE SUCH */
+/* AS OPPOSED TO HAVING A BUNCH OF PUBLIC METHODS */
+
+public class PHONE_LINES 
+{
+     public static String SCAN_NEXT_LINE;
+     public static String[] LINE_DATA;
+     public static String PHONE_BRAND;
+     public static String PHONE_MODEL;
+     public static String PHONE_ANNOUNCED;
+     public static String PHONE_MEM;
+     public static float PHONE_WEIGHT;
+     public static int PHONE_PRICE;
+
+     public PHONE_LINES()
+     {
+
+     }
+}
